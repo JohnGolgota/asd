9550e0bea26b8d3533bd49f03cee46ce9362e67b
JohnGolgota <104475729+JohnGolgota@users.noreply.github.com>
2024-04-09 14:37:57 -0500

    chore: data source
    la fuente de la info

--- setup.ps1
+++ setup.ps1
@@ -1,22 +1,6 @@
 
-# Obtener la ruta del directorio donde se encuentra el script en ejecuci—n
-$scriptRoot = $PSScriptRoot
+Write-Host "Make data directory"
+# create data directory
+New-Item -ItemType File -Path ${HOME}\.repos.csv -ErrorAction SilentlyContinue
 
-# Construir la ruta completa del directorio 'bin' dentro del directorio del script
-$binDirectory = Join-Path -Path $scriptRoot -ChildPath 'bin'
-
-# Obtener el valor actual del PATH del Registro de PowerShell
-$currentPath = [System.Environment]::GetEnvironmentVariable("PATH", "Machine")
-
-# Agregar el directorio 'bin' al PATH si no est‡ presente ya
-if ($currentPath -notlike "*$binDirectory*")
-{
-    $newPath = "$currentPath;$binDirectory"
-    [System.Environment]::SetEnvironmentVariable("PATH", $newPath, "Machine")
-    
-    Write-Host "Se ha agregado '$binDirectory' al PATH del Registro de PowerShell correctamente."
-} else
-{
-    Write-Host "'$binDirectory' ya esta presente en el PATH del Registro de PowerShell."
-}
-
+Write-Host "you need add bin directory to PATH manually"
