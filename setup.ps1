c788e7271237fa6d51cf3ba8e59daddafcb781c0
John Steban Lopez <104475729+JohnGolgota@users.noreply.github.com>
2024-04-12 13:42:10 -0500

    feat: utils ps1

--- setup.ps1
+++ setup.ps1
@@ -1,22 +1,31 @@
 
-# Obtener la ruta del directorio donde se encuentra el script en ejecuci—n
+# Obtener la ruta del directorio donde se encuentra el script en ejecuciï¿½n
 $scriptRoot = $PSScriptRoot
 
 # Construir la ruta completa del directorio 'bin' dentro del directorio del script
 $binDirectory = Join-Path -Path $scriptRoot -ChildPath 'bin'
 
+. $binDirectory\Repo.Utils.ps1
+
+Test-RepoDataPath
+
 # Obtener el valor actual del PATH del Registro de PowerShell
 $currentPath = [System.Environment]::GetEnvironmentVariable("PATH", "Machine")
 
-# Agregar el directorio 'bin' al PATH si no est‡ presente ya
-if ($currentPath -notlike "*$binDirectory*")
-{
+# Agregar el directorio 'bin' al PATH si no esta presente ya
+if ($currentPath -notlike "*$binDirectory*") {
     $newPath = "$currentPath;$binDirectory"
-    [System.Environment]::SetEnvironmentVariable("PATH", $newPath, "Machine")
+
+    [Environment]::SetEnvironmentVariable('PATH', $newPath, 'Machine')
     
     Write-Host "Se ha agregado '$binDirectory' al PATH del Registro de PowerShell correctamente."
-} else
-{
+
+    Add-Content -Path $PROFILE -Value @"
+. Repo.Utils.ps1
+"@
+
+}
+else {
     Write-Host "'$binDirectory' ya esta presente en el PATH del Registro de PowerShell."
 }
 
