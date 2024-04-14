d89922be175051d84ae6b7681cad0c1cd76467d5
John Steban Lopez <104475729+JohnGolgota@users.noreply.github.com>
2024-04-14 23:54:32 +0000

    porque hace eso?

--- bin/Repo.Utils.sh
+++ bin/Repo.Utils.sh
@@ -7,32 +7,18 @@ touch $archivo_csv
 # Crear un array asociativo en Zsh para almacenar los datos
 declare -A datos
 
-# Ejecutar awk y almacenar los datos en un archivo temporal
-awk -F ',' '{
-    clave=$1
-    valor=$2
-    gsub(/^ *| *$/,"",valor)
-    datos[clave]=valor
-} END {
-    for (i in datos) {
-        print i, datos[i]
-    }
-}' "$archivo_csv" > temp_file
-
 # Leer los datos del archivo temporal y almacenarlos en el array asociativo
-while read -r clave valor; do
-    datos[$clave]=$valor
-done < temp_file
-# rm temp_file
+while read -r linea; do
+    parts=(${(@s:,:)linea})
+    datos[$parts[1]]=${parts[2]}
+done < $archivo_csv
 
-echo '_bb() {
-    claves=(' >> nuevo.sh
-echo ${(@k)datos} >> nuevo.sh
-echo ')
+_bb() {
+    claves=(${(@k)datos})
     compadd "$claves[@]"
 }
 bb() {
     echo "${datos[$1]}"
-}' >> nuevo.sh
+}
 
 compdef _bb bb
